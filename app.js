deb0d1db009cc160b16ebfe0256a93de70f69ba5
sumitIO <sumitk25a@gmail.com>
2020-05-18 15:39:32 +0530

    INITIAL COMMIT

---
+++ app.js
@@ -0,0 +1,107 @@
+new Vue ({
+    el: '#app',
+    data : {
+        playerHealth: 100,
+        monsterHealth: 100,
+        gameIsRunning: false,
+        maxHeal: 3,
+        maxSpecialAttack: 3,
+        turns: []
+    },
+    methods: {
+        startGame() {
+            this.gameIsRunning = true;
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+            this.maxHeal = 3,
+            this.maxSpecialAttack = 3,
+            this.turns = [];
+        },
+        attack() {
+            var damage = this.calcDamage(10,3);
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits monster for ' + damage + ' units'
+            });
+            if(this.checkWin()) {
+                return;
+            }
+            this.monsterAttack();
+        },
+        specialAttack() {
+            if(this.maxSpecialAttack > 0) {
+
+                this.maxSpecialAttack -=1;
+                var damage = this.calcDamage(20,10);
+                this.monsterHealth -= damage;
+                this.turns.unshift({
+                    isPlayer: true,
+                    text: 'Player hits Monster hard for ' + damage + ' units'
+                });
+
+                if(this.checkWin()) {
+                    return;
+                }
+                this.monsterAttack();
+            }            
+        },
+        heal() {
+            if(this.maxHeal > 0){
+                this.maxHeal -=1;
+                if( this.playerHealth <=90) {
+                    this.playerHealth +=10;
+                } else {
+                    this.playerHealth =100;
+                }
+                this.monsterAttack();
+            }
+            
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player healed for 10 units'
+            })
+        },
+        giveUp() {
+            this.gameIsRunning = false;
+            alert('MONSTER WON!!!\nYou left the game');
+        },
+        monsterAttack() {
+            var damage = this.calcDamage(12,5)
+            this.playerHealth -= damage;
+            
+            this.turns.unshift({
+                isPlayer: false,
+                text: 'Monster hits player for ' + damage + ' units'
+            });
+
+            this.checkWin();
+
+            
+        },
+        calcDamage(MAX,MIN) {
+            return Math.max(Math.floor(Math.random() * MAX) + 1, MIN);
+        },
+        checkWin() {
+            if(this.monsterHealth <=0) {
+                this.monsterHealth = 0;
+                if(confirm('You WON! New Game?')) {
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+                return true;
+            }
+            else if(this.playerHealth <=0) {
+                this.playerHealth = 0;
+                if(confirm('You LOST! New Game?')) {
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+                return true;
+            }
+            return false;
+        },
+    }
+});
